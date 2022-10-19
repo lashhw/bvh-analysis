54104100e08d9effca7a7bef86ef631c993c0aaa
Jacky Huang <lashhw81@gmail.com>
2022-10-19 22:01:23 +0800

    add newline

--- utils/build_bvh.hpp
+++ utils/build_bvh.hpp
@@ -13,7 +13,7 @@ Bvh build_bvh(const std::vector<Triangle> &triangles) {
     auto global_bbox = bvh::compute_bounding_boxes_union(bboxes.get(), triangles.size());
     std::cout << "global bounding box: ("
               << global_bbox.min[0] << ", " << global_bbox.min[1] << ", " << global_bbox.min[2] << "), ("
-              << global_bbox.max[0] << ", " << global_bbox.max[1] << ", " << global_bbox.max[2] << ")";
+              << global_bbox.max[0] << ", " << global_bbox.max[1] << ", " << global_bbox.max[2] << ")" << std::endl;
 
     bvh::SweepSahBuilder<Bvh> builder(bvh);
     builder.build(global_bbox, bboxes.get(), centers.get(), triangles.size());
