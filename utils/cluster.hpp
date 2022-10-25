24db08d432b6a93b67561619804312ea5954aef6
Jacky Huang <lashhw81@gmail.com>
2022-10-26 00:20:47 +0800

    change to preorder

--- utils/cluster.hpp
+++ utils/cluster.hpp
@@ -1,10 +1,10 @@
 #ifndef BVH_ANALYSIS_CLUSTER_HPP
 #define BVH_ANALYSIS_CLUSTER_HPP
 
-void inorder_cluster(const Bvh &bvh, size_t nodes_per_cluster,
-                     size_t curr_node_idx,
-                     size_t &counter,
-                     std::unordered_map<size_t, size_t> &node_idx_to_cluster_idx) {
+void preorder_cluster(const Bvh &bvh, size_t nodes_per_cluster,
+                      size_t curr_node_idx,
+                      size_t &counter,
+                      std::unordered_map<size_t, size_t> &node_idx_to_cluster_idx) {
     if (!bvh.nodes[curr_node_idx].is_leaf()) {
         size_t left_idx = bvh.nodes[curr_node_idx].first_child_or_primitive;
         size_t right_idx = left_idx + 1;
@@ -13,12 +13,11 @@ void inorder_cluster(const Bvh &bvh, size_t nodes_per_cluster,
             std::swap(left_idx, right_idx);
         }
 
-        inorder_cluster(bvh, nodes_per_cluster, left_idx, counter, node_idx_to_cluster_idx);
-
         node_idx_to_cluster_idx[curr_node_idx] = counter / nodes_per_cluster;
         counter++;
 
-        inorder_cluster(bvh, nodes_per_cluster, right_idx, counter, node_idx_to_cluster_idx);
+        preorder_cluster(bvh, nodes_per_cluster, left_idx, counter, node_idx_to_cluster_idx);
+        preorder_cluster(bvh, nodes_per_cluster, right_idx, counter, node_idx_to_cluster_idx);
     } else {
         node_idx_to_cluster_idx[curr_node_idx] = counter / nodes_per_cluster;
         counter++;
@@ -29,7 +28,7 @@ std::unordered_map<size_t, size_t> cluster(const Bvh &bvh, size_t nodes_per_clus
     std::unordered_map<size_t, size_t> node_idx_to_cluster_idx;
 
     size_t counter = 0;
-    inorder_cluster(bvh, nodes_per_cluster, 0, counter, node_idx_to_cluster_idx);
+    preorder_cluster(bvh, nodes_per_cluster, 0, counter, node_idx_to_cluster_idx);
 
     return node_idx_to_cluster_idx;
 }
