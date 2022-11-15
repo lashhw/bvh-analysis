387915e6fc89a513aa7ec45d3735e98dc045fe51
Jacky Huang <lashhw81@gmail.com>
2022-11-15 17:00:00 +0800

    32bits per data

--- gen_coe_file/main.cpp
+++ gen_coe_file/main.cpp
@@ -15,31 +15,45 @@ int main() {
     nodes_coe_file << "memory_initialization_radix = 16;\nmemory_initialization_vector =\n";
     for (int i = 0; i < bvh.node_count; i++) {
         const Bvh::Node &node = bvh.nodes[i];
-        if (i != 0) nodes_coe_file << ",\n";
-        for (int j = 0; j < 6; j++)
+        for (int j = 0; j < 6; j++) {
             nodes_coe_file << std::setfill('0') << std::setw(8) << std::hex << float_as_unsigned_int(node.bounds[j]);
+            nodes_coe_file << ",\n";
+        }
         nodes_coe_file << std::setfill('0') << std::setw(8) << std::hex << node.primitive_count;
+        nodes_coe_file << ",\n";
         nodes_coe_file << std::setfill('0') << std::setw(8) << std::hex << node.first_child_or_primitive;
+        nodes_coe_file << ",\n";
     }
-    nodes_coe_file << ';';
+    nodes_coe_file << "0;";
 
     std::ofstream triangles_coe_file("triangles.coe");
     triangles_coe_file << "memory_initialization_radix = 16;\nmemory_initialization_vector =\n";
     for (int i = 0; i < triangles.size(); i++) {
         const Triangle &triangle = triangles[bvh.primitive_indices[i]];
-        if (i != 0) triangles_coe_file << ",\n";
         triangles_coe_file << std::setfill('0') << std::setw(8) << std::hex << float_as_unsigned_int(triangle.p0[0]);
+        triangles_coe_file << ",\n";
         triangles_coe_file << std::setfill('0') << std::setw(8) << std::hex << float_as_unsigned_int(triangle.p0[1]);
+        triangles_coe_file << ",\n";
         triangles_coe_file << std::setfill('0') << std::setw(8) << std::hex << float_as_unsigned_int(triangle.p0[2]);
+        triangles_coe_file << ",\n";
         triangles_coe_file << std::setfill('0') << std::setw(8) << std::hex << float_as_unsigned_int(triangle.e1[0]);
+        triangles_coe_file << ",\n";
         triangles_coe_file << std::setfill('0') << std::setw(8) << std::hex << float_as_unsigned_int(triangle.e1[1]);
+        triangles_coe_file << ",\n";
         triangles_coe_file << std::setfill('0') << std::setw(8) << std::hex << float_as_unsigned_int(triangle.e1[2]);
+        triangles_coe_file << ",\n";
         triangles_coe_file << std::setfill('0') << std::setw(8) << std::hex << float_as_unsigned_int(triangle.e2[0]);
+        triangles_coe_file << ",\n";
         triangles_coe_file << std::setfill('0') << std::setw(8) << std::hex << float_as_unsigned_int(triangle.e2[1]);
+        triangles_coe_file << ",\n";
         triangles_coe_file << std::setfill('0') << std::setw(8) << std::hex << float_as_unsigned_int(triangle.e2[2]);
+        triangles_coe_file << ",\n";
         triangles_coe_file << std::setfill('0') << std::setw(8) << std::hex << float_as_unsigned_int(triangle.n[0]);
+        triangles_coe_file << ",\n";
         triangles_coe_file << std::setfill('0') << std::setw(8) << std::hex << float_as_unsigned_int(triangle.n[1]);
+        triangles_coe_file << ",\n";
         triangles_coe_file << std::setfill('0') << std::setw(8) << std::hex << float_as_unsigned_int(triangle.n[2]);
+        triangles_coe_file << ",\n";
     }
-    triangles_coe_file << ';';
+    triangles_coe_file << "0;";
 }
